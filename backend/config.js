0c8c3143de4226ad332714941fc388391932481f
Chaitanyea <119304080+Chaitanyea@users.noreply.github.com>
2024-05-06 19:49:27 +0530

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,2 +1,2 @@
 export const PORT = 5050;
-export const MONGO_URL = "mongodb+srv://FinKraft:FinKraft@cluster0.tqdtmvt.mongodb.net/?retryWrites=true&w=majority";
+export const MONGO_URL = "mongodb+srv://<user>:pass@cluster0.tqdtmvt.mongodb.net/?retryWrites=true&w=majority";
